50583a5af45013c2dad8393d3a770bb6cb737286
Akash Kushwaha <kushwahaakash2000@gmail.com>
2025-03-25 00:49:10 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   console.log("regenerate page");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Example of SSR</h1>
+      <h1>Example of ISR</h1>
       <h3>Date time is {dateTime.toString()}</h3>
     </div>
   );
